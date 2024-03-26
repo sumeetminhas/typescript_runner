14ad29a1aa16b2427bc970f90883a0b5f1f59cb3
Sumeet <sminhas39@my.bcit.ca>
2024-03-26 13:56:29 -0700

    line and circle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-alert("hi there")
 
-const div = document.querySelector("#root")
-console.log("root", div)
+const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+
+const ctx = canvas.getContext('2d');
+
+function drawCircle(x: number, y: number, radius: number) {
+  ctx?.beginPath();
+  ctx?.arc(x, y, radius, 0, Math.PI * 2);
+  ctx?.closePath();
+  ctx?.stroke();
+}
+
+function drawLine(y: number, x1: number, x2: number) {
+  ctx?.beginPath();
+  ctx?.moveTo(x1, y);
+  ctx?.lineTo(x2, y);
+  ctx?.stroke();
+}
+
+drawCircle(100, 100, 25);
+drawLine(150, 50, 800);
