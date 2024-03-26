9071c8f4b1ea6960bf45064c7e8220d7bbc6429d
Sumeet <sminhas39@my.bcit.ca>
2024-03-26 14:49:41 -0700

    circle moves on keydown left and right across screen

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
 
 const ctx = canvas.getContext('2d');
 
+let circleX = 100;
+const circleY = 100;
+const radius = 25;
+const speed = 5;
+
 function drawCircle(x: number, y: number, radius: number) {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height)
   ctx?.beginPath();
   ctx?.arc(x, y, radius, 0, Math.PI * 2);
   ctx?.closePath();
   ctx?.stroke();
 }
+
 
 function drawLine(y: number, x1: number, x2: number) {
   ctx?.beginPath();
@@ -17,5 +24,22 @@ function drawLine(y: number, x1: number, x2: number) {
   ctx?.stroke();
 }
 
-drawCircle(100, 100, 25);
-drawLine(150, 50, 800);
+function animate() {
+  requestAnimationFrame(animate)
+  drawCircle(circleX, circleY, radius)
+  drawLine(150, 50, 800);
+}
+
+// canvas.addEventListener('click', function(e) {
+//   circleX = e.clientX - canvas.getBoundingClientRect().left; 
+// })
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === "ArrowRight") {
+    circleX = Math.min(canvas.width - radius, circleX + speed);
+  } else if (event.key === "ArrowLeft") {
+    circleX = Math.max(radius, circleX - speed)
+  }
+})
+
+animate()
