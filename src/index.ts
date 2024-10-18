f9f8d939b5b243efbb2bcea57334f06a3b96d513
Sumeet <sminhas39@my.bcit.ca>
2024-10-17 20:22:08 -0700

    collision detection and nodemon and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
-
 const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
 
 const ctx = canvas.getContext('2d');
 
-type Phase = { coord: { x: number; y: number }, id: number, name: string, description: string, speed: number }
+type Phase = {
+	coord: { x: number; y: number };
+	id: number;
+	name: string;
+	description: string;
+	speed: number;
+  isColliding: boolean;
+};
 
 const phases: Phase[] = [
-  { coord: {x: canvas.width - 50, y:  150},
-    id: 1, 
-    name: "menstrual", 
-    description: "something about menstrual phase",
-    speed: 6
-  }, { 
-    coord: {x: canvas.width - 50, y: 150},
-    id: 2, 
-    name: "follicular", 
-    description: "something about follicular phase",
-    speed: 4
-  }, {
-    coord: {x: canvas.width - 50, y: 150}, 
-    id: 3, 
-    name: "ovulation", 
-    description: "something about ovulation phase",
-    speed: 2
-  }, { 
-    coord: {x: canvas.width - 50, y: 150},
-    id: 4, 
-    name: "luteal", 
-    description: "something about luteal phase",
-    speed: 1
-  }
-]
+	{
+		coord: { x: canvas.width - 50, y: 149 },
+		id: 1,
+		name: 'menstrual',
+		description: 'something about menstrual phase',
+		speed: 6,
+    isColliding: false
+	},
+	{
+		coord: { x: canvas.width - 50, y: 149 },
+		id: 2,
+		name: 'follicular',
+		description: 'something about follicular phase',
+		speed: 4,
+    isColliding: false
+	},
+	{
+		coord: { x: canvas.width - 50, y: 149 },
+		id: 3,
+		name: 'ovulation',
+		description: 'something about ovulation phase',
+		speed: 2,
+    isColliding: false
+	},
+	{
+		coord: { x: canvas.width - 50, y: 149 },
+		id: 4,
+		name: 'luteal',
+		description: 'something about luteal phase',
+		speed: 1,
+    isColliding: false
+	},
+];
 
 let circleX = 100;
 let circleY = 100;
@@ -39,142 +53,146 @@ const moveSpeed = 1.5;
 const groundSpeed = 2;
 let isJumping = false;
 const jumpSpeed = 5;
-let isColliding = false;
-let collisionID : null | number = null;
+let pause = false;
+let collisionID: null | number = null;
 
 function drawCircle(x: number, y: number, radius: number) {
-  ctx?.beginPath();
-  ctx?.arc(x, y, radius, 0, Math.PI * 2);
-  ctx?.closePath();
-  ctx?.stroke();
+	ctx?.beginPath();
+	ctx?.arc(x, y, radius, 0, Math.PI * 2);
+	ctx?.closePath();
+	ctx?.stroke();
 }
 
 function drawGroundCircle(x: number, y: number, radius: number) {
-  ctx?.beginPath();
-  ctx?.arc(x, y, radius, 0, Math.PI * 2);
-  ctx?.closePath();
-  ctx?.stroke();
+	ctx?.beginPath();
+	ctx?.arc(x, y, radius, 0, Math.PI * 2);
+	ctx?.closePath();
+	ctx?.stroke();
 }
 
 function drawLine(y: number, x1: number, x2: number) {
-  ctx?.beginPath();
-  ctx?.moveTo(x1, y);
-  ctx?.lineTo(x2, y);
-  ctx?.stroke();
+	ctx?.beginPath();
+	ctx?.moveTo(x1, y);
+	ctx?.lineTo(x2, y);
+	ctx?.stroke();
 }
 
 function animate() {
-  if (!isColliding) {
-    requestAnimationFrame(animate)
-    ctx?.clearRect(0, 0, canvas.width, canvas.height)
-  
-    if (!ctx) {
-      return;
-    }
-  
-  // // move ground circle
-  //     groundCircleX -= groundSpeed;
-  //     if (groundCircleX <= -50) {
-  //       groundCircleX = canvas.width - 50
-  //     }
+	if (!pause) {
+		requestAnimationFrame(animate);
+		ctx?.clearRect(0, 0, canvas.width, canvas.height);
 
-  phases.forEach(phase => {
-    phase.coord.x -= phase.speed
-    if (phase.coord.x <= -50) {
-      phase.coord.x = canvas.width - 50;
-    }
-    drawCircle(phase.coord.x, phase.coord.y, radius)
-  })
+		if (!ctx) {
+			return;
+		}
 
-  //move top circle
-  circleX += moveSpeed;
-  if (circleX >= canvas.width - radius) {
-    circleX = -radius
-  }
+		// // move ground circle
+		//     groundCircleX -= groundSpeed;
+		//     if (groundCircleX <= -50) {
+		//       groundCircleX = canvas.width - 50
+		//     }
 
-    collisionDetection();
-    drawCircle(circleX, circleY, radius)
-    // drawGroundCircle(groundCircleX, 150, 10)
-    drawLine(150, 50, 800);
+		phases.forEach((phase) => {
+			phase.coord.x -= phase.speed;
+			if (phase.coord.x <= -50) {
+				phase.coord.x = canvas.width - 50;
+			}
+			drawCircle(phase.coord.x, phase.coord.y, radius);
+		});
 
-  } else {
-    const phase = phases.find((p) => p.id === collisionID )
-    if (phase) {
-      ctx?.beginPath()
-      ctx?.rect(20, 20, 200, 200)
-      ctx?.stroke();
-      ctx?.fillText(phase.name, 30, 50)
-      ctx?.fillText(phase.description, 30, 70)
-    }
-  }
+		//move top circle
+		circleX += moveSpeed;
+		if (circleX >= canvas.width - radius) {
+			circleX = -radius;
+		}
+
+		collisionDetection();
+		drawCircle(circleX, circleY, radius);
+		// drawGroundCircle(groundCircleX, 150, 10)
+		drawLine(150, 50, 800);
+	} else {
+		const phase = phases.find((p) => p.id === collisionID);
+		if (phase) {
+			ctx?.beginPath();
+			ctx?.rect(20, 20, 200, 200);
+			ctx?.stroke();
+			ctx?.fillText(phase.name, 30, 50);
+			ctx?.fillText(phase.description, 30, 70);
+		}
+	}
 }
 
-
-
 function jumpUp() {
-  const jumpInterval = setInterval(function() {
-    circleY = Math.max(0, circleY - jumpSpeed);
-    if (circleY === 0) {
-      clearInterval(jumpInterval);
-      descend();
-    }
-  }, 16);
+	const jumpInterval = setInterval(function () {
+		circleY = Math.max(0, circleY - jumpSpeed);
+		if (circleY === 0) {
+			clearInterval(jumpInterval);
+			descend();
+		}
+	}, 16);
 
-  function descend() {
-    const descendInterval = setInterval(function() {
-      circleY = Math.min(100, circleY + jumpSpeed);
-      if (circleY === 100) {
-        clearInterval(descendInterval);
-      }
-    }, 16);
-  }    
+	function descend() {
+		const descendInterval = setInterval(function () {
+			circleY = Math.min(100, circleY + jumpSpeed);
+			if (circleY === 100) {
+				clearInterval(descendInterval);
+			}
+		}, 16);
+	}
 }
 
 function collisionDetection() {
-  for (let i = 0; i < phases.length; i++) {
-    // const distanceX = Math.abs(circleX - groundCircleX);
-    const distanceX = Math.abs(circleX - phases[i].coord.x);
-    if (distanceX <= radius * 2) {
-      const distanceY = Math.abs(circleY - phases[i].coord.y);
-      if (distanceY <= radius * 2) {
-        isColliding = true;
-        collisionID = phases[i].id
-        console.log("collision id", collisionID)
+	for (let i = 0; i < phases.length; i++) {
+		// const distanceX = Math.abs(circleX - groundCircleX);
+		const distanceX = Math.abs(circleX - phases[i].coord.x);
+    const distanceY = Math.abs(circleY - phases[i].coord.y);
+    const distance = Math.sqrt(distanceX**2 + distanceY**2)
+
+    if (distance <= radius + radius) {
+      if (!phases[i].isColliding){
+        pause = true;
+        collisionID = phases[i].id;
+        phases[i].isColliding = true;
+        console.log('collision id', collisionID);
         break;
       }
-      // numberOfCollisions += 1
-    } else {
-      isColliding = false;
-    }  
-  }
+    }
+		// if (distanceX <= radius * 2) {
+		// 	if (distanceY <= radius * 2) {
+
+		// 	}
+			// numberOfCollisions += 1
+		else {
+			pause = false;
+      phases[i].isColliding = false;
+		}
+	}
 }
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'i') {
-    if (isColliding) {
-      isColliding = false;
-      collisionID = null;
-      animate()
-    }
-  }
-})
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'i') {
+		if (pause) {
+			pause = false;
+			collisionID = null;
+			animate();
+		}
+	}
+});
 
-document.addEventListener('keydown', function(e) {
-  if (e.key === ' ') {
-    jumpUp();
-  }
-})
+document.addEventListener('keydown', function (e) {
+	if (e.key === ' ') {
+		jumpUp();
+	}
+});
 
-animate()
-
-
+animate();
 
 // canvas.addEventListener('click', function(e) {
-//   circleX = e.clientX - canvas.getBoundingClientRect().left; 
+//   circleX = e.clientX - canvas.getBoundingClientRect().left;
 // })
 
 // *****************************************************************************
-// **** code to move circle left, right, and jump up when pressing spacebar **** 
+// **** code to move circle left, right, and jump up when pressing spacebar ****
 // document.addEventListener('keydown', function(event) {
 //   if (event.key === "ArrowRight") {
 //     circleX = Math.min(canvas.width - radius, circleX + moveSpeed);
@@ -185,5 +203,3 @@ animate()
 //   }
 // })
 // *******************************************************************************
-
-
